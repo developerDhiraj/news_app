6a9144ff37ec23101c567c10c66f2be852dbaaff
developerDhiraj <dhirajbarku@gmail.com>
2025-02-25 08:48:19 +0530

    update

---
+++ lib/model.dart
@@ -0,0 +1,20 @@
+class NewsQueryModel
+{
+  late String newsHead;
+  late String newsDes;
+  late String newsImg;
+  late String newsUrl;
+
+  NewsQueryModel ({
+    this.newsHead = "News Headline", this.newsDes = "News Description", this.newsImg = "News Image", this.newsUrl = "News Url"
+});
+  factory NewsQueryModel.fromMap(Map news)
+  {
+    return NewsQueryModel(
+      newsHead: news['title'],
+      newsDes: news['description'],
+      newsImg: news['urlToImage'],
+      newsUrl: news['url'],
+    );
+  }
+}
